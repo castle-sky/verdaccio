151472de320ba4677e221e3de8520f22ecc93f45
Juan Picado <juanpicado19@gmail.com>
2021-05-17 22:42:49 +0200

    chore: fix warnings

--- packages/core/server/src/server.ts
+++ packages/core/server/src/server.ts
@@ -3,7 +3,7 @@ import fastify from 'fastify';
 async function startServer() {
   const app = fastify();
 
-  app.get('/', async (request, reply) => {
+  app.get('/', async () => {
     return { hello: 'world' };
   });
 
